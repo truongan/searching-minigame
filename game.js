b0eb99e9dd285aa228a7a4d57a1a00cd45b8104b
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2017-02-15 07:39:45 +0700

    more editting

--- game.js
+++ game.js
@@ -11,17 +11,28 @@ function new_game(){
 	console.log(ordered);
 	var size = $("#size").val();
 	var x = 0;
-	var increase_factor = 3;
+	var increase_factor = Math.floor( Math.random() * 4+1);
 	$("#placeholder_for_buttons").html("");
+	$("#x_value").html(String(Math.floor( Math.random() * increase_factor * size)));
+	var exist = false;
 	for (var i = 1; i <= size; i++) {
-		if (ordered) x += Math.floor( Math.random() * (increase_factor *1.5));
+		if (ordered) x += Math.floor( Math.random() * (increase_factor *1.85));
 		else x = Math.floor( Math.random() * increase_factor * size);
+		if (x == $("#x_value").text()) exist = true;
 		$("#placeholder_for_buttons").append(
 			'<button type="button" data-value="' + x + '" class="btn btn-default btn-lg buttonbox"><br/><span class="badge">' + fix_width(i, increase_factor * size) +'</span>'
 		);
 	}
-	$("#x_value").html(String(Math.floor( Math.random() * increase_factor * size)));
-	$(".buttonbox").click(function(btn){
+
+	$("#not_exist").click(function(exist){
+		if(exist){
+			$(".modal-title").html("WRONG");
+			$(".modal-body > p").html("That value is in there somewhere")
+			$('#myModal').modal(options)
+		}
+	});
+
+	$(".buttonbox").click(function(){
 		if ($(this).hasClass('active')) return ;
 
 		$("#score").text( parseInt($("#score").text()) + 1);
@@ -31,14 +42,21 @@ function new_game(){
 		a.removeClass('active');
 		console.log(a);
 		$(this).addClass('active');
-		$(this).prepend('<span class="label label-primary">' +  $(this).data('value') + '</span>' );
+		$(this).prepend('<span class="label label-danger">' +  $(this).data('value') + '</span>' );
 	});
 }
 $(document).ready(function(){
     $("form").submit(function(e){
         e.preventDefault();
     });
+
+	$('#myModal').on('shown.bs.modal', function () {
+	  $('#myInput').focus()
+	})
+
 	$("#ordered").change(new_game);
 	$("#size").change(new_game);
 	new_game();
+
+
 });
