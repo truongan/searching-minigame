43873d1bca5c03a3e58a8c07b048a8852bb37a0c
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2018-01-29 16:54:53 +0700

    fixing bug while migrate to bs4

--- game.js
+++ game.js
@@ -6,19 +6,22 @@ function fix_width(i, size){
 	}
 	return i;
 }
+exist = false;
 function new_game(){
 	var ordered = $("#ordered").is(':checked');
-	console.log(ordered);
+	
 	var size = $("#size").val();
 	var x = 0;
-	var increase_factor = Math.floor( Math.random() * 4+1);
+	var increase_factor = Math.floor( Math.random() * 3+1);
 	//$("#placeholder_for_buttons").html("");
 	$("#x_value").html(String(Math.floor( Math.random() * increase_factor * size)));
-	var exist = false;
+	exist = false;
 	for (var i = 1; i <= size; i++) {
 		if (ordered) x += Math.floor( Math.random() * (increase_factor *1.85));
 		else x = Math.floor( Math.random() * increase_factor * size);
 		if (x == $("#x_value").text()) exist = true;
+		
+
 		$("#placeholder_for_buttons").append(
 			'<button type="button" data-value="' 
 				+ x 
@@ -30,11 +33,15 @@ function new_game(){
 		);
 	}
 
-	$("#not_exist").click(function(exist){
+	$("#not_exist").click(function(){
 		if(exist){
 			$(".modal-title").html("WRONG");
-			$(".modal-body > p").html("That value is in there somewhere")
-			$('#myModal').modal(options)
+			$(".modal-body ").html("That value is in there somewhere")
+			$('#myModal').modal()
+		} else {
+			$(".modal-title").html("Congratulation");
+			$(".modal-body ").html("That value indeed DOES NOT appear in any box")
+			$('#myModal').modal()
 		}
 	});
 
@@ -49,6 +56,12 @@ function new_game(){
 		console.log(a);
 		$(this).addClass('active');
 		$(this).prepend('<span class="badge badge-pill badge-danger">' +  $(this).data('value') + '</span>' );
+
+		if ($(this).data('value') == $("#x_value").text()){
+			$(".modal-title").html("Congratulation");
+			$(".modal-body ").html("You have found the box contain that special value")
+			$('#myModal').modal()
+		}
 	});
 }
 $(document).ready(function(){
