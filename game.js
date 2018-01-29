313d7dcb7a41b5473a423765bd7d124b3413891d
truongan@acer_extensa_m2610 <truongan@acer_extensa_m2610>
2018-01-29 16:00:57 +0700

    update to bs4

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ function new_game(){
 	var size = $("#size").val();
 	var x = 0;
 	var increase_factor = Math.floor( Math.random() * 4+1);
-	$("#placeholder_for_buttons").html("");
+	//$("#placeholder_for_buttons").html("");
 	$("#x_value").html(String(Math.floor( Math.random() * increase_factor * size)));
 	var exist = false;
 	for (var i = 1; i <= size; i++) {
@@ -20,7 +20,13 @@ function new_game(){
 		else x = Math.floor( Math.random() * increase_factor * size);
 		if (x == $("#x_value").text()) exist = true;
 		$("#placeholder_for_buttons").append(
-			'<button type="button" data-value="' + x + '" class="btn btn-default btn-lg buttonbox"><br/><span class="badge">' + fix_width(i, increase_factor * size) +'</span>'
+			'<button type="button" data-value="' 
+				+ x 
+				+ '" class="btn btn-primary btn-lg buttonbox">'
+				+'<br/><span class="badge badge-secondary">' 
+				+ fix_width(i, increase_factor * size) 
+				+'</span>'
+				+'</button>'
 		);
 	}
 
@@ -38,11 +44,11 @@ function new_game(){
 		$("#score").text( parseInt($("#score").text()) + 1);
 		var a = $("button.active");
 		console.log(a);
-		$("button > span.label").remove();
+		$("button > span.badge-pill").remove();
 		a.removeClass('active');
 		console.log(a);
 		$(this).addClass('active');
-		$(this).prepend('<span class="label label-danger">' +  $(this).data('value') + '</span>' );
+		$(this).prepend('<span class="badge badge-pill badge-danger">' +  $(this).data('value') + '</span>' );
 	});
 }
 $(document).ready(function(){
