cd933e5d219aa85bdec41e407fc5e7a42278ab07
an <hostmaster@truongan.name.vn>
2018-01-29 18:44:54 +0700

    Fix spacing and typo issue

--- game.js
+++ game.js
@@ -9,27 +9,27 @@ function fix_width(i, size){
 exist = false;
 function new_game(){
 	var ordered = $("#ordered").is(':checked');
-	
+
 	var size = $("#size").val();
 	var x = 0;
 	var increase_factor = Math.floor( Math.random() * 3+1);
-	//$("#placeholder_for_buttons").html("");
+	$("#placeholder_for_buttons").html("");
 	$("#x_value").html(String(Math.floor( Math.random() * increase_factor * size)));
 	exist = false;
 	for (var i = 1; i <= size; i++) {
 		if (ordered) x += Math.floor( Math.random() * (increase_factor *1.85));
 		else x = Math.floor( Math.random() * increase_factor * size);
 		if (x == $("#x_value").text()) exist = true;
-		
+
 
 		$("#placeholder_for_buttons").append(
-			'<button type="button" data-value="' 
-				+ x 
-				+ '" class="btn btn-primary btn-lg buttonbox">'
-				+'<br/><span class="badge badge-secondary">' 
-				+ fix_width(i, increase_factor * size) 
-				+'</span>'
-				+'</button>'
+			'<button type="button" data-value="'
+				+ x
+				+ '" class="btn btn-primary btn-lg mt-1 buttonbox">'
+				+'<br/><span class=""><kbd>'
+				+ fix_width(i, increase_factor * size)
+				+'</samp></kbd>'
+				+'</button> ' //There HAVE TO BE ONE SPACE after button tag otherwise bootstrap will mess up the spacing
 		);
 	}
 
@@ -76,6 +76,6 @@ $(document).ready(function(){
 	$("#ordered").change(new_game);
 	$("#size").change(new_game);
 	new_game();
-
+	//$("#placeholder_for_buttons").append('<button type="button" class="btn btn-lg btn-primary">Primary</button> <button type="button" class="btn btn-lg btn-secondary">Secondary</button> <button type="button" class="btn btn-lg btn-success">Success</button><button type="button" class="btn btn-lg btn-danger">Danger</button>');
 
 });
